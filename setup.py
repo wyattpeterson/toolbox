37380ba54805510ced53990935d3a3ebd4044ccb
Wyatt <wyatt.peterson@gmail.com>
2013-01-26 11:46:58 -0800

    remove vim setup

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 import sys
 import os
 from pprint import pprint as pp
-from subprocess import check_call
 import shutil
 
 
@@ -16,18 +15,10 @@ def install_dotfiles(repo):
     dotfiles_dir = os.path.join(repo, 'dotfiles')
     dotfiles = [os.path.join(dotfiles_dir, dotfile) for dotfile in os.listdir(dotfiles_dir)]
     map(install_dotfile, dotfiles)
-        
-def install_vim(repo):
-    src = os.path.join(repo, 'vim')
-    dst = os.path.expanduser("~/.vim")
-    print "Copying %s to %s" % (src, dst)
-    shutil.copytree(src,dst)
-
 
 def main():
     repo=os.path.dirname(os.path.realpath(__file__))
     install_dotfiles(repo)
-    install_vim(repo)
     return 0
 
 if __name__ == '__main__':
