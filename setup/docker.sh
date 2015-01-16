af87204eceb78b0e3e8ddc0a04fd70c90107b47c
Wyatt <wpeterson328@users.noreply.github.com>
2015-01-16 14:46:26 -0800

    Update docker.sh

--- setup/docker.sh
+++ setup/docker.sh
@@ -10,7 +10,10 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8
 echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list
 #apt-get update
 apt-get install lxc-docker cgroup-lite apparmor 
-service docker restart
+
 
 #add my user to the docker group so i don't have to sudo all the time
 usermod -a -G docker wyatt
+
+
+service docker restart
