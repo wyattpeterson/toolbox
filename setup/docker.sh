028bc3d48a4dddd1ab1134a3205a828f4d892b91
Wyatt <wpeterson328@users.noreply.github.com>
2015-01-16 14:57:50 -0800

    Update docker.sh

--- setup/docker.sh
+++ setup/docker.sh
@@ -8,7 +8,7 @@ set -o nounset
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 
 echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list
-#apt-get update
+apt-get update
 apt-get install lxc-docker cgroup-lite apparmor 
 
 
