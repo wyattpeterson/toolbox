ab3962a488ffb7395b062468c65f7f73d947c93f
Wyatt <wyatt.peterson@gmail.com>
2015-01-16 12:50:40 -0800

    update initial setup scripts

--- setup/config.sh
+++ setup/config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-#cp -r ~/toolbox/config/* ~/.config
+cp -r ~/toolbox/config/* ~/.config
 
 git config --local user.email "wyatt.peterson@gmail.com"
 git config --local user.name "Wyatt"
+git config --global push.default matching
