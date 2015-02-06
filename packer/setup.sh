ff614be0269daff17dac74213be8bf9ef31b28b7
Wyatt <wpeterson328@users.noreply.github.com>
2015-02-06 13:27:11 -0800

    Update setup.sh

--- packer/setup.sh
+++ packer/setup.sh
@@ -3,7 +3,10 @@ set -o nounset
 set -o errexit
 set -x
 
-LOCAL_IPV4=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+
+#if you're not using aws assigned private dns names in your vpc you may need this
+#apt-get is sad if it can't resolve it's own hostname
+#LOCAL_IPV4=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 #echo -e "$LOCAL_IPV4 $(hostname) \n127.0.0.1 localhost" > /etc/hosts
 
 export DEBIAN_FRONTEND=noninteractive
