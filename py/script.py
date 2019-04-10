bb2328d15d9e0ec1ffa98027482f4c53e57aeda1
Wyatt Peterson <wpeterson@nerdwallet.com>
2019-04-09 18:17:29 -0700

    make script template python3

--- py/script.py
+++ py/script.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 import sys
 
 def main():
-    print "This is a script template"
+    print("This is a script template")
     return 0
 
 if __name__ == '__main__':
