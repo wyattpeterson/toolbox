c9b88d934f3dfdb79a872a93bd808920cf129782
Wyatt <wpeterson328@users.noreply.github.com>
2020-05-24 16:23:02 -0700

    Update setup (#3)
    * Update setup.py to py3
    
    * add gitignore

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#/usr/bin/env python3
 # encoding: utf-8
 import sys
 import os
@@ -8,29 +8,19 @@ import shutil
 
 def install_dotfile(dotfile):
     destination = os.path.expanduser("~/.%s" % os.path.basename(dotfile))
-    print "Copying %s to %s" % (dotfile, destination)
+    print("Copying {} to {}".format(dotfile, destination))
     shutil.copyfile(dotfile, destination)
 
 def install_dotfiles(repo):
     dotfiles_dir = os.path.join(repo, 'dotfiles')
     dotfiles = [os.path.join(dotfiles_dir, dotfile) for dotfile in os.listdir(dotfiles_dir)]
-    map(install_dotfile, dotfiles)
+    for dotfile in dotfiles:
+        install_dotfile(dotfile)
+
 
 def main():
-#    home = os.path.expanduser("~")
-#    bashrcd = os.path.join(home, ".bashrc.d")
-#    if not os.path.exists(bashrcd):
-#        os.makedirs(bashrcd)
-
     repo=os.path.dirname(os.path.realpath(__file__))
     install_dotfiles(repo)
-
-#to-do install files
-#vim plugin problems
-#reload xfce4 settings without reload
-
-
-
     return 0
 
 if __name__ == '__main__':
